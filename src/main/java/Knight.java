66a1cc2e8ae6f064b98cdd2cec54c8a061d619d1
vladkyr <vlkyryk@gmail.com>
2020-05-11 23:10:48 +0300

    adjustments to variables and comments in piece classes

--- src/main/java/Knight.java
+++ src/main/java/Knight.java
@@ -10,25 +10,23 @@ public class Knight extends Piece{
 
     @Override
     public abstract boolean isAppropriateMove(Cell destination) {
-        Pair<short, short> curPos = super.cell.getPosition();
-        curX = curPos.getValue0();
-        curY = curPos.getValue1();
-        Pair<short, short> desPos = destination.getPosition();
-        desX = desPos.getValue0();
-        desY = desPos.getValue1();
+        Pair<int, int> curPos = super.cell.getPosition();
+        int curX = curPos.getValue0();
+        int curY = curPos.getValue1();
+        Pair<int, int> desPos = destination.getPosition();
+        int desX = desPos.getValue0();
+        int desY = desPos.getValue1();
         boolean isOccupied = destination.getOccupation();
-        moveX = Math.abs(desX - curX);
-        moveY = Math.abs(desY - curY);
+        int moveX = Math.abs(desX - curX);
+        int moveY = Math.abs(desY - curY);
 
         if( (moveX == 1 && moveY == 2) || (moveX == 2 && moveY == 1) ){
             if(!isOccupied){
-                //movePiece
-                return 1;
+                return 1; //movePiece
             }
             else{
                 if(super.getColourAsString() != destination.piece.getColourAsString()){
-                    //movePiece to beat
-                    return 1;
+                    return 1; //movePiece to beat
                 }
             }
         }
--- src/main/java/Pawn.java
+++ src/main/java/Pawn.java
@@ -10,12 +10,12 @@ public class Pawn extends Piece{
 
     @Override
     public abstract boolean isAppropriateMove(Cell destination) {
-        Pair<short, short> curPos = super.cell.getPosition();
-        curX = curPos.getValue0();
-        curY = curPos.getValue1();
-        Pair<short, short> desPos = destination.getPosition();
-        desX = desPos.getValue0();
-        desY = desPos.getValue1();
+        Pair<int, int> curPos = super.cell.getPosition();
+        int curX = curPos.getValue0();
+        int curY = curPos.getValue1();
+        Pair<int, int> desPos = destination.getPosition();
+        int desX = desPos.getValue0();
+        int desY = desPos.getValue1();
         boolean isOccupied = destination.getOccupation();
         boolean isOccupiedByWhite = destination.piece.getColourAsString() == "White" ? 1 : 0;
 
@@ -23,36 +23,32 @@ public class Pawn extends Piece{
             if (curX = desX) {
                 if ( (curY == 1 && desY == 3) && !isOccupied){ // pawn's first move
                     if(super.checkIfPathIsClear(super.cell, destination)) // there is no other piece between current pos and destination
-                        //movePiece
-                        return 1;
+                        return 1; //movePiece
                 }
                 if( (desY-curY == 1) && !isOccupied){
-                    //movePiece
-                    return 1;
+                    return 1; //movePiece
                 }
             }
             if( ((desX-curX == 1) || (desX-curX == -1)) && (desY-curY == 1) && isOccupied ){ //beats other piece
                 if(!isOccupiedByWhite)
-                    //movePiece to beat
-                    return 1;
+                    return 1; //movePiece to beat
             }
         }
         if(super.cell.piece.getColourAsString() == "Black") {
             if (curX = desX) {
-                if ( (curY == 6 && desY == 4) && !isOccupied){ // pawn's first move
-                    if(super.checkIfPathIsClear(super.cell, destination)) // there is no other piece between current pos and destination
-                    //movePiece
-                        return 1;
+                if ( (curY == 6 && desY == 4) && !isOccupied) { // pawn's first move
+                    if (super.checkIfPathIsClear(super.cell, destination)) { // there is no other piece between current pos and destination
+                        return 1; //movePiece
+                    }
                 }
-                if( (desY-curY == -1) && !isOccupied){
-                    //movePiece
-                    return 1;
+                if( (desY-curY == -1) && !isOccupied) {
+                    return 1; //movePiece
                 }
             }
-            if( ((desX-curX == 1) || (desX-curX == -1)) && (desY-curY == -1) && isOccupied ){ //beats other piece
-                if(isOccupiedByWhite)
-                    //movePiece to beat
-                    return 1;
+            if( ((desX-curX == 1) || (desX-curX == -1)) && (desY-curY == -1) && isOccupied ) { //beats other piece
+                if(isOccupiedByWhite) {
+                    return 1; //movePiece to beat
+                }
             }
         }
         return 0;
