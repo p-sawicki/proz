082d7b5dec55171e16e299fb6bf3f25a323d999b
psawicki0 <01143606@pw.edu.pl>
2020-05-24 17:13:31 +0200

    Resource paths now OS-independent.

---
+++ src/main/java/Utility.java
@@ -0,0 +1,7 @@
+import java.io.File;
+
+public class Utility {
+    static String getResourcePath(){
+        return "src" + File.separator + "main" + File.separator + "resources" + File.separator;
+    }
+}
